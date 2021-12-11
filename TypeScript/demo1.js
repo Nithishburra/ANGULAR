bb74c87ffd6950b7e74b3cb14ef735abf08ae28f
Nithish <Nithishgoudb@gmail.com>
2021-12-11 22:11:59 +0530

    code

---
+++ TypeScript/demo1.js
@@ -0,0 +1,28 @@
+
+let str = "HELLO"
+
+let newstr = str.split('').reverse('').join('')
+
+console.log(newstr);
+
+
+
+
+
+let str1 = "welcome to java"
+
+let newstr1 = str1.split('').reverse('').join('')
+console.log(newstr1);
+
+
+
+const str2 = "hey hello"
+let newstr2 =''
+for(let i = str2.length-1; i>=0; i--){
+  
+     newstr2 += str2[i];
+}
+
+console.log(newstr2);
+
+
